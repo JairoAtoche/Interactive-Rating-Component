bae485e89a083e4daacf03a6f593927104c7a502
Jairo Atoche <jairoatoche90098@gmail.com>
2022-07-05 17:35:46 -0500

    add cambio de estado a los botones

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 var boton = document.querySelectorAll(".button-list__button");
 let ratingContent = document.querySelector(".main__rating-container");
 let submitContent = document.querySelector(".main__submit-container");
+let rating = document.querySelector(".submit__span");
+
 
 boton.forEach(btn => {
-    btn.addEventListener("click",evt=>{
-        btn.style.backgroundColor ="red";
-    })
-    
-})
-
-
-document.getElementById("btn_submit").addEventListener("click",()=>{
-    ratingContent.style.display = "none";
-    submitContent.style.display = "inherit";
+    btn.onclick = () => {
+        boton.forEach(btn => {
+            /* Quitar estado a todos los botones */
+            btn.classList.remove('button-list__button--activo');
+        });
+        /* Agregar estado del boton */
+        btn.classList.add('button-list__button--activo');
+        rating.innerHTML = btn.innerHTML;
+        /* Cambiar de pantalla  */
+        document.getElementById("btn_submit").addEventListener("click",()=>{
+            ratingContent.style.display = "none";
+            submitContent.style.display = "inherit";
+        });
+    }
 });
